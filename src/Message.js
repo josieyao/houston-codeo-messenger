58cd532363475a5480da5ab4808c9f5740179154
josieyao <yao.josephine@yahoo.com>
2019-04-12 05:25:07 -0500

    Done

--- src/Message.js
+++ src/Message.js
@@ -1,31 +1,30 @@
 class Message {
-    constructor(message){
+    constructor(message){ 
         this.message = message
-        // this.createMsg(this)--instance method
 
-        const msgListSection = document.querySelector('#messages') //find the ul section
-        const msgListItem = document.createElement('li') //create a new li
+        const msgListSection = document.querySelector('#messages') 
+        const msgListItem = document.createElement('li')
         // console.log(message) 
-        msgListItem.innerText = message["content"] //this & message is the same thing
+        msgListItem.innerText = message.content
         msgListSection.append(msgListItem)
     }
 
-    static createMsg(message){ //class method to create messages for the whole class
-        fetch(`${messagesURL}`, { //need to interpolate the messagesURL
+    static createMsg(message){ 
+        fetch(`${messagesURL}`, { 
             method: "POST",
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                'Accept': 'application/json', 
+                'Content-Type': 'application/json' 
             },
             body: JSON.stringify({
-                content: message //message needs to match the argument being passed in
+                content: message 
             })
         })
         .then(function(response) {
             return response.json()
         })
         .then(function(response){
-            new Message(response) //create a new message with the response given 
+            new Message(response) 
         })
     }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const messagesURL = `http://10.185.1.104:3000/messages`
 
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { 
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  setInterval( () => {  //refreshes the page every 5 seconds
-    fetch(messagesURL) //fetches the existing database
+  setInterval( () => {  
+    fetch(messagesURL)
     .then(function(response){
-      return response.json()
+      return response.json() 
     })
     .then(function(messages){
-      const msgListSection = document.querySelector('#messages') //finds the ul section
-      msgListSection.innerText = " " //resets the page to " " because setInterval will refresh the page every 5 secs
+      const msgListSection = document.querySelector('#messages') 
+      msgListSection.innerText = " " 
       messages.forEach(function(message){ 
         message = new Message(message)
       })
@@ -19,12 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 500)
   
 
-  const form = document.querySelector('#message_form') //finding the whole form
-  const msgInput = document.querySelector('#message_input') //finding the text box where the user inputs their msg
-  form.addEventListener('submit', (e) => { //listening on a submit to the form
+  const form = document.querySelector('#message_form') 
+  const msgInput = document.querySelector('#message_input') 
+  form.addEventListener('submit', (e) => { 
       e.preventDefault()
-      Message.createMsg(msgInput.value) //acting on the whole class of Message to create a message with what the user inputted
-      form.reset() //resets the form to blank
+      Message.createMsg(msgInput.value) 
+      form.reset() 
   })
 
 })
